046f01cad877056fc05ff1dd37cd78edb131fb1b
marinabrovko <marishka.brovko@gmail.com>
2025-01-14 17:08:46 +0300

    add views

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from lms.models import Course, Lesson
+from lms.serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+class LessonCreateView(generics.CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class LessonListView(generics.ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDetailView(generics.RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateView(generics.UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDeleteView(generics.DestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
