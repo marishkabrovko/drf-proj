879eb0c281155a1aac543e8054d6b052c43fcbb5
marinabrovko <marishka.brovko@gmail.com>
2025-01-19 23:23:00 +0300

    add payment

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,13 +3,19 @@ from rest_framework import serializers
 from lms.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = ["name", "description"]
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
-        fields = ["name", "description", "video_link", "course"]
+        fields = ["id", "name", "description", "video_link", "course"]
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField(read_only=True)
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = ["id", "name", "description", "lessons_count", "lessons"]
