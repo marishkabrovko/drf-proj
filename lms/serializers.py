e9f2efe4ddc17538591bc5a24d5667755e44bcf7
marinabrovko <marishka.brovko@gmail.com>
2025-01-30 13:42:15 +0300

    validate_video_link

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 
 from lms.models import Course, Lesson
+from lms.validators import validate_video_link
 
 
 class LessonSerializer(serializers.ModelSerializer):
+
+    video_link = serializers.CharField(validators=[validate_video_link])
+
     class Meta:
         model = Lesson
         fields = ["id", "name", "description", "video_link", "course"]
