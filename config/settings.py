e6343e13883d4128a9691b18ca3f483e243316d6
marinabrovko <marishka.brovko@gmail.com>
2025-02-04 16:21:33 +0300

    celery,redis,tasks,mail

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
+    'django_celery_beat',
     "drf_yasg",
     "users",
     "lms",
@@ -108,6 +109,15 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
 
 REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
@@ -123,3 +133,25 @@ SIMPLE_JWT = {
 }
 
 STRIPE_APIKEY = os.getenv("STRIPE_APIKEY")
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+CELERY_TIMEZONE = TIME_ZONE
+
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+CELERY_BEAT_SCHEDULE = {
+    'task-name': {
+        'task': 'myapp.tasks.my_task',
+        'schedule': timedelta(days=1),
+    },
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+    }
+}
--- lms/views.py
+++ lms/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from lms.tasks import send_mail_about_update_course
 from lms.models import Course, Lesson, Subscription
 from lms.paginators import LessonCoursePagination
 from lms.serializers import CourseSerializer, LessonSerializer
@@ -27,6 +28,10 @@ class CourseViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         course = serializer.save(owner=self.request.user)
+
+    def perform_update(self, serializer):
+        course = serializer.save()
+        send_mail_about_update_course.delay(course_id=course.pk)
 
 
 class LessonCreateView(generics.CreateAPIView):
@@ -67,13 +72,13 @@ class SubscriptionAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        user = request.user  # ✅ Теперь это объект User
+        user = request.user
         course_id = request.data.get("course")
 
         if not course_id:
             return Response({"error": "Поле 'course' обязательно"}, status=400)
 
-        course = get_object_or_404(Course, pk=course_id)  # ✅ Теперь это объект Course
+        course = get_object_or_404(Course, pk=course_id)
 
         subscription = Subscription.objects.filter(user=user, course=course)
 
@@ -81,7 +86,7 @@ class SubscriptionAPIView(APIView):
             subscription.delete()
             message = "Подписка удалена"
         else:
-            Subscription.objects.create(user=user, course=course)  # ✅ Передаём объекты
+            Subscription.objects.create(user=user, course=course)
             message = "Подписка добавлена"
 
         return Response({"message": message}, status=200)
