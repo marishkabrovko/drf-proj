a5ee71a24f322ac649c7b66e3e1a5830dd225074
marinabrovko <marishka.brovko@gmail.com>
2025-02-04 16:59:06 +0300

    celery beat, task for users

--- config/settings.py
+++ config/settings.py
@@ -134,8 +134,8 @@ SIMPLE_JWT = {
 
 STRIPE_APIKEY = os.getenv("STRIPE_APIKEY")
 
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
 
 CELERY_TIMEZONE = TIME_ZONE
 
@@ -143,8 +143,8 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
 CELERY_BEAT_SCHEDULE = {
-    'task-name': {
-        'task': 'myapp.tasks.my_task',
+    'check_last_login': {
+        'task': 'users.tasks.check_last_login',
         'schedule': timedelta(days=1),
     },
 }
