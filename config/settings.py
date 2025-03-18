3985866891ef7105f1e33559e4a82996b7d9dfb7
marinabrovko <marishka.brovko@gmail.com>
2025-03-18 22:34:56 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -64,16 +64,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 
-if 'test' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'test_db.sqlite3',
-        }
-    }
-
-else:
-    DATABASES = {
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.getenv('POSTGRES_DB'),
@@ -167,3 +158,11 @@ CACHES = {
         "LOCATION": "redis://redis:6379/0",
     }
 }
+
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'test_db.sqlite3',
+        }
+    }
