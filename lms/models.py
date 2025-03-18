ef261d75a66e241c35b583342baac65d780e6c01
marinabrovko <marishka.brovko@gmail.com>
2025-03-18 20:49:56 +0300

    test3

--- lms/models.py
+++ lms/models.py
@@ -4,31 +4,13 @@ from users.models import User
 
 
 class Course(models.Model):
-    name = models.CharField(
-        max_length=100, verbose_name="Курс", help_text="Введите название курса"
-    )
-    description = models.TextField(
-        verbose_name="Описание курса",
-        help_text="Введите описание курса",
-        blank=True,
-        null=True,
-    )
-    preview = models.ImageField(
-        upload_to="media/img/",
-        verbose_name="Превью курса",
-        help_text="Загрузите превью курса",
-        blank=True,
-        null=True,
-    )
-    owner = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        verbose_name="Владелец",
-        help_text="Выберите владельца курса",
-        related_name="courses",
-        null=True,
-        blank=True,
-    )
+    name = models.CharField(max_length=100, verbose_name="Курс", help_text="Введите название курса")
+    description = models.TextField(verbose_name="Описание курса", help_text="Введите описание курса", blank=True,
+                                   null=True)
+    preview = models.ImageField(upload_to="media/img/", verbose_name="Превью курса",
+                                help_text="Загрузите превью курса", blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Владелец",
+                              help_text="Выберите владельца курса", related_name="courses", null=True, blank=True)
 
     class Meta:
         verbose_name = "Курс"
@@ -39,46 +21,16 @@ class Course(models.Model):
 
 
 class Lesson(models.Model):
-    name = models.CharField(
-        max_length=100, verbose_name="Урок", help_text="Введите название урока"
-    )
-    description = models.TextField(
-        verbose_name="Описание урока",
-        help_text="Введите описание урока",
-        blank=True,
-        null=True,
-    )
-    preview = models.ImageField(
-        upload_to="media/img/",
-        verbose_name="Превью урока",
-        help_text="Загрузите превью урока",
-        blank=True,
-        null=True,
-    )
-    video_link = models.CharField(
-        max_length=255,
-        verbose_name="Ссылка на урок", help_text="Добавьте ссылку на урок",
-        null=True,
-        blank=True,
-    )
-    course = models.ForeignKey(
-        Course,
-        on_delete=models.SET_NULL,
-        verbose_name="Курс",
-        help_text="Выберите курс",
-        related_name="lessons",
-        blank=True,
-        null=True,
-    )
-    owner = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        verbose_name="Владелец",
-        help_text="Выберите владельца урока",
-        related_name="lessons",
-        null=True,
-        blank=True,
-    )
+    name = models.CharField(max_length=100, verbose_name="Урок", help_text="Введите название урока")
+    description = models.TextField(verbose_name="Описание урока", help_text="Введите описание урока", blank=True,
+                                   null=True)
+    preview = models.ImageField(upload_to="media/img/", verbose_name="Превью урока",
+                                help_text="Загрузите превью урока", blank=True, null=True)
+    video_link = models.CharField(max_length=255, verbose_name="Ссылка на урок", help_text="Добавьте ссылку на урок")
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name="Курс", help_text="Выберите курс",
+                               related_name="lessons", blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Владелец",
+                              help_text="Выберите владельца урока", related_name="lessons", null=True, blank=True)
 
     class Meta:
         verbose_name = "Урок"
@@ -89,18 +41,8 @@ class Lesson(models.Model):
 
 
 class Subscription(models.Model):
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        verbose_name="Пользователь",
-        related_name="subscriptions",
-    )
-    course = models.ForeignKey(
-        Course,
-        on_delete=models.CASCADE,
-        verbose_name="Курс",
-        related_name="subscriptions",
-    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь", related_name="subscriptions")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Курс", related_name="subscriptions")
     date = models.DateField(verbose_name="Дата подписки", auto_now_add=True)
 
     class Meta:
